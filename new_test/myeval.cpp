c6ea78e2fefc1b797141ee056bf3ddb0109a5057
rakshith_harish <62082190+rak94@users.noreply.github.com>
2021-03-08 12:05:27 -0600

    Update myeval.cpp

--- new_test/myeval.cpp
+++ new_test/myeval.cpp
@@ -1,31 +1,31 @@
-double eval(int *key){
+double eval(int *pj){
   int sum = 0;
   int count = 0;
   
   for(count = 0; count < 33; count++){
-    if(key[count] > 0){
+    if(pj[count] > 0){
       sum++;
     }
   }
   
   for(count = 33; count < 36; count++){
-    if(key[count] < 1){
+    if(pj[count] < 1){
       sum++;
     }
   }
   
   for(count = 36; count < 70; count++){
-    if(key[count] > 0){
+    if(pj[count] > 0){
       sum++;
     }
   }
   
-  if(key[count] > 0 && key[count+1] > 0){
+  if(pj[count] > 0 && pj[count+1] > 0){
     sum += 2;
   }
   
   for(count = 72;count < 100;count++){
-    if(key[count] < 1){
+    if(pj[count] < 1){
       sum++;
     }
   }
